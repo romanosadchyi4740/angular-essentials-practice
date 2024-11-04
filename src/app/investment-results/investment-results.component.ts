f3472a2392eccf0d5809a10d75621831b7c2f1bd
Roman Osadchyi <romanos4740@gmail.com>
2024-11-04 13:04:24 +0200

    Standalone components

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,5 +1,6 @@
-import {Component, input} from '@angular/core';
+import {Component, computed, inject} from '@angular/core';
 import {CurrencyPipe} from "@angular/common";
+import {InvestmentService} from "../investment.service";
 
 @Component({
   selector: 'app-investment-results',
@@ -11,13 +12,8 @@ import {CurrencyPipe} from "@angular/common";
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-  // results = input()
-  results = input<{
-    year: number;
-    interest: number;
-    valueEndOfYear: number;
-    annualInvestment: number;
-    totalInterest: number;
-    totalAmountInvested: number;
-  }[]>();
+  private investmentService = inject(InvestmentService);
+
+  results = computed(() =>
+    this.investmentService.resultData);
 }
